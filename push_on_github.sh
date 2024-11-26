b3f9eecaa85dd38474a24c7c2d7a1f4ed8ca85ba
Ga1maz <aganiev806@gmail.com>
2024-11-26 21:56:02 +0300

    V(1)

--- push_on_github.sh
+++ push_on_github.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
+# Определяем путь к каталогу, где находится скрипт
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Функция для получения количества запусков скрипта
+get_run_count() {
+  count_file="${script_dir}/run_count.txt"
+  
+  if [ ! -f "$count_file" ]; then
+    echo 0 > "$count_file"
+  fi
+  
+  run_count=$(cat "$count_file")
+  ((run_count++))
+  echo $run_count > "$count_file"
+  
+  echo $run_count
+}
+
+# Функция для добавления всех изменённых файлов в индекс
 add_files() {
   echo "Добавляю изменённые файлы..."
   git add . > /dev/null 2>&1
 }
 
+# Функция для создания коммита
 commit_changes() {
-  read -p "Введите сообщение для коммита: " commit_message
+  run_count=$(get_run_count)
+  commit_message="V($run_count)"
   echo "Делаем коммит с сообщением: $commit_message"
   git commit -m "$commit_message" > /dev/null 2>&1
 }
-й
+
+# Функция для отправки изменений на удалённый репозиторий
 push_to_remote() {
   echo "Отправляю изменения на удалённый репозиторий..."
   git push origin master > /dev/null 2>&1
 }
 
+# Основная логика скрипта
 echo "Проверяю статус репозитория..."
 git_status=$(git status --porcelain)
 if [[ -n "$git_status" ]]; then
