f0ff8d5d4deb56290d5ff0dc0bb7fc439eb493e4
Ga1maz <aganiev806@gmail.com>
2024-11-26 21:56:36 +0300

    V(2)

--- scripts/libraries/misc.sh
+++ scripts/libraries/misc.sh
@@ -2,7 +2,7 @@
 #
 # This script has been created as part of the Blueprint source code
 # and uses the same license as the rest of the codebase.
-
+# 
 
 # === DATABASE ===
 FLDR=".blueprint/extensions/blueprint/private/db/database"
