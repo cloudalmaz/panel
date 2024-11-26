6b1d02f493e442c8052b97242e700baf693f82a1
Ga1maz <aganiev806@gmail.com>
2024-11-26 21:53:02 +0300

    V0.0

---
+++ push_on_github.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+add_files() {
+  echo "Добавляю изменённые файлы..."
+  git add . > /dev/null 2>&1
+}
+
+commit_changes() {
+  read -p "Введите сообщение для коммита: " commit_message
+  echo "Делаем коммит с сообщением: $commit_message"
+  git commit -m "$commit_message" > /dev/null 2>&1
+}
+й
+push_to_remote() {
+  echo "Отправляю изменения на удалённый репозиторий..."
+  git push origin main > /dev/null 2>&1
+}
+
+echo "Проверяю статус репозитория..."
+git_status=$(git status --porcelain)
+if [[ -n "$git_status" ]]; then
+  add_files
+  commit_changes
+  push_to_remote
+else
+  echo "Нет изменений для коммита."
+fi
